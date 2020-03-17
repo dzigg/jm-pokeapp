4b253fc255dc721072f8d7c953727a5008dee6a5
dzigg <dfoxyz@live.com>
2020-03-17 12:23:29 +1000

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="pika-bg">
+    <nuxt />
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.pika-bg {
+  @apply min-h-screen;
+  background: url(../assets/img/bg.svg) #f4f4f4;
+  background-position: center;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <div class="bg-red-400">
+      Nah maybe
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
